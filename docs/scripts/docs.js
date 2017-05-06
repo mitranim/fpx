15a76f888b68f6e75a705fab73cc2f5d41ca8e0e
Nelo Mitranim <me@mitranim.com>
2017-05-06 17:57:41 +0300

    docs: reliable scroll spill prevention

--- docs/scripts/docs.js
+++ docs/scripts/docs.js
@@ -1,60 +1,51 @@
 const {
-  location,
   history,
-  fpx: {
-    and, bind, curry1, rest, getAt, seq, pipe, id: exists, ifelse, ifthen,
-    slice, find
-  },
+  fpx: {bind, get, and, truthy, procure, slice, isFunction, validate},
 } = window
 
-// Elems
+/**
+ * Utils
+ */
 
-const sidenav = document.getElementById('sidenav')
+// Pixel measurements are inaccurate when the browser is zoomed in or out, so we
+// have to use a small non-zero value in some geometry checks.
+const PX_ERROR_MARGIN = 3
 
-const main = document.getElementById('main')
+class Throttle {
+  constructor (fun, options) {
+    validate(isFunction, fun)
+    this.fun = fun
+    this.options = options
+    this.timerId = null
+    this.tailPending = false
+  }
 
-const links = slice(sidenav.getElementsByTagName('a'))
+  run () {
+    if (this.timerId) this.tailPending = true
+    else restartThrottle.call(this)
+  }
 
-const targets = main.querySelectorAll('[id]:not([id=""])')
+  isPending () {
+    return Boolean(this.timerId)
+  }
 
-// Utils
-
-const getf = rest(curry1(getAt))
-
-const hasVisibleId = and(exists, tangible, withinViewport, getf('id'))
-
-const findId = pipe(bind(find, hasVisibleId, targets), getf('id'))
-
-const ifexists = bind(ifthen, exists)
-
-const getLink = ifexists(function getLink (id) {
-  return find(bind(matchHref, id), links)
-})
-
-const updateHash = ifelse(exists, setHash, unsetHash)
-
-function linksOff () {links.forEach(deactivate)}
-
-const reactivate = ifexists(seq(activate, scrollIntoViewIfNeeded))
-
-const update = pipe(findId, seq(updateHash, linksOff, pipe(getLink, reactivate)))
-
-const reachedScrollEdge = ifexists(function reachedScrollEdge (elem, {deltaY}) {
-  return (
-    deltaY < 0 && reachedTop(elem) ||
-    deltaY > 0 && reachedBottom(elem)
-  )
-})
-
-const reachedTop = ifexists(function reachedTop ({scrollTop}) {return scrollTop < 3})
-
-const reachedBottom = ifexists(function reachedBottom (elem) {
-  return Math.abs(elem.scrollHeight - absBottom(elem)) < 3
-})
-
-function absBottom (elem) {
-  return elem.getBoundingClientRect().height + elem.scrollTop
+  stop () {
+    clearTimeout(this.timerId)
+    this.timerId = null
+  }
 }
+
+function restartThrottle () {
+  this.stop()
+  this.timerId = setTimeout(() => {
+    this.timerId = null
+    if (this.tailPending) restartThrottle.call(this)
+    this.tailPending = false
+    this.fun(...arguments)
+  }, get(this.options, 'delay'))
+}
+
+const getVisibleId = and(truthy, hasArea, withinViewport, elem => elem.id)
 
 function findParent (test, node) {
   return !node ? null : (test(node) ? node : findParent(test, node.parentNode))
@@ -64,7 +55,7 @@ function hasAttr (name, elem) {
   return elem && elem.hasAttribute && elem.hasAttribute(name)
 }
 
-function tangible (elem) {
+function hasArea (elem) {
   const {height, width} = elem.getBoundingClientRect()
   return height > 0 && width > 0
 }
@@ -72,9 +63,68 @@ function tangible (elem) {
 function withinViewport (elem) {
   const {top, bottom} = elem.getBoundingClientRect()
   return (
-    bottom > -3 && bottom < window.innerHeight ||
-    top > 3 && top < (window.innerHeight + 3)
+    bottom > -PX_ERROR_MARGIN && bottom < window.innerHeight ||
+    top > PX_ERROR_MARGIN && top < (window.innerHeight + PX_ERROR_MARGIN)
   )
+}
+
+function setHash (id) {
+  history.replaceState(null, '', `#${id}`)
+}
+
+function unsetHash () {
+  history.replaceState(null, '', '')
+}
+
+function scrollIntoViewIfNeeded (elem) {
+  if (!withinViewport(elem)) elem.scrollIntoView()
+}
+
+function preventScrollSpill (elem, event) {
+  event.preventDefault()
+  elem.scrollLeft += event.deltaX
+  elem.scrollTop += event.deltaY
+}
+
+/**
+ * Init
+ */
+
+const scroller = new Throttle(updateLinksAndHash, {delay: 250})
+
+window.addEventListener('scroll', scroller.run.bind(scroller))
+
+const shouldPreventSpill = bind(hasAttr, 'data-nospill')
+
+window.addEventListener('wheel', function preventSpill (event) {
+  const node = findParent(shouldPreventSpill, event.target)
+  if (node) preventScrollSpill(node, event)
+})
+
+// window.addEventListener('popstate', function toHash () {
+//   scroller.stop()
+//   const hash = window.location.hash.replace(/^#/, '')
+//   if (hash) updateSidenavLinks(hash)
+// })
+
+function updateLinksAndHash () {
+  const id = procure(getVisibleId, document.querySelectorAll('#main [id]'))
+  if (id) {
+    setHash(id)
+    updateSidenavLinks(id)
+  }
+  else {
+    unsetHash(id)
+  }
+}
+
+function updateSidenavLinks (id) {
+  slice(document.querySelectorAll('#sidenav a.active')).forEach(deactivate)
+  const link = document.querySelector(`#sidenav a[href*="#${id}"]`)
+  if (link) {
+    activate(link)
+    scrollIntoViewIfNeeded(link)
+  }
 }
 
 function activate (elem) {
@@ -84,52 +134,3 @@ function activate (elem) {
 function deactivate (elem) {
   elem.classList.remove('active')
 }
-
-function matchHref (id, link) {
-  return !!~link.href.indexOf('#' + id)
-}
-
-function setHash (id) {
-  history.replaceState('', null, `${location.origin}${location.pathname}#${id}`)
-}
-
-function unsetHash () {
-  history.replaceState('', null, location.origin + location.pathname)
-}
-
-function scrollIntoViewIfNeeded (elem) {
-  if (!withinViewport(elem)) elem.scrollIntoView()
-}
-
-function throttle (fun, delay) {
-  let id
-  let tail = false
-
-  function start () {
-    return setInterval(function runThrottled () {
-      if (!tail) {
-        clearInterval(id)
-        id = null
-      }
-      tail = false
-      fun(...arguments)
-    }, delay)
-  }
-
-  return function throttled () {
-    if (!id) id = start(...arguments)
-    else if (!tail) tail = true
-  }
-}
-
-// Setup
-
-window.addEventListener('scroll', throttle(update, 250))
-
-const hasNoSpill = bind(hasAttr, 'data-nospill')
-
-window.addEventListener('wheel', function preventSpill (event) {
-  if (reachedScrollEdge(findParent(hasNoSpill, event.target), event)) {
-    event.preventDefault()
-  }
-})
